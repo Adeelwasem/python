87db708a219af802fdd123f34a61a1c415e0d4d6
Adeelwasem <waseemadeelsabbir@gmail.com>
2025-07-11 14:49:18 +0600

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,17 @@
+age = 15
+
+weight = 38.5
+
+
+print("Before TypeCasting data type" , type (age))
+newage = str(age)
+print()
+
+
+print("Data Type of age after typecasting is" , type (newage))
+print()
+
+
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is " , type(weight))
