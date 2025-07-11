7f4119bdf9d2b2d5a9532f14c62ecdb36ce9fb5b
Adeelwasem <waseemadeelsabbir@gmail.com>
2025-07-11 14:22:32 +0600

    Add files via upload

---
+++ data.py
@@ -0,0 +1,11 @@
+a = 5
+print("type of a:" , type (a))
+
+b = 2.5
+print("type of b:" , type (b))
+
+c = "Sofia"
+print("type of c:" , type (c))
+
+isMale = False
+print("type of d:" , type (isMale))
