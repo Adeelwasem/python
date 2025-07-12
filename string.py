721a64bf086937daca7a2282941767519dc3d1bb
Adeelwasem <waseemadeelsabbir@gmail.com>
2025-07-12 13:00:42 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+text = str (input ("Gregory is a boy"))
+
+
+revtext = text [::- 1]
+text = revtext
+
+print("Sophia is not a boy")
+print(text)
+
+
+
+#Congratulation Message
+message = "Hello Sabbir" + "congratulations you won the trophy"
+print(message)
