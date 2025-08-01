4e7ea9ed72a08580a7c26a36b5b66b82c7b63c9e
Adeelwasem <waseemadeelsabbir@gmail.com>
2025-08-01 14:33:00 +0600

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,18 @@
+V = 4
+W = 5
+X = 8
+y = 2
+z = 0
+
+
+z = (V + W) * X / y
+print("Value of (V + W) * X / y is" , z)
+
+
+name = "Alex"
+age = 0
+
+if name == "Alex" or (name == "John" and age >= 2):
+    print("Hello!Welcome")
+else:
+    print("Good Bye!!")
