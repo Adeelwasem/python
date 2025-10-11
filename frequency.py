16ecdf9808c3882fcd8d53ec491d8a61cc2d6c46
Adeelwasem <waseemadeelsabbir@gmail.com>
2025-10-11 18:08:38 +0600

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,22 @@
+test_dict = {
+
+    'Codingal' :  2,
+    'is' :  2,
+    'best' :  2,
+    'for' :  2,
+    'coding' :  1,
+
+}
+
+print("The original dictionary :" +  str(test_dict))
+
+VALUE = 2
+RESULT = 0
+
+for key in test_dict:
+
+    if test_dict [key] == VALUE:
+         RESULT = RESULT + 1
+
+
+print("Frequency of K is :" +  str(RESULT))
