265d2f08fd09ce844ec51eff17fba88961f9eec1
Adeelwasem <waseemadeelsabbir@gmail.com>
2025-12-12 13:54:52 +0600

    Add files via upload

---
+++ romanconverter.py
@@ -0,0 +1,11 @@
+num1 = 45
+roman1 = converter.int_to_roman(num1)
+print(f"The Roman numeral for {num1} is: {roman1}")
+
+num2 = 70
+roman2 = converter.int_to_roman(num2)
+print(f"The Roman numeral for {num2} is: {roman2}")
+
+num3 = 11
+roman3 = converter.int_to_roman(num3)
+print(f"The Roman numeral for {num3} is: {roman3}")
